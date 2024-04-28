6bb24b4bb7a2e4412cb2d6631852e7fd84cead64
DanielFGray <danielfgray@gmail.com>
2024-04-28 16:17:35 -0500

    add: react aria file browser

--- src/components/FileBrowser.tsx
+++ src/components/FileBrowser.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import clsx from "classnames";
-import { classed } from "@tw-classed/react";
 import {
   getFileList,
   useSelector,
@@ -9,75 +8,88 @@ import {
   newFile,
   getCurrentFile,
 } from "../store";
+import {
+  Input,
+  ListBox,
+  ListBoxItem,
+} from "react-aria-components";
 import { Button } from ".";
 
-export const FileNode = classed(
-  "button",
-  "tracking-light block w-full p-1 text-left text-sm outline-1 outline-primary-300 hover:outline",
-  {
-    variants: {
-      state: {
-        selected: 'bg-primary-700 text-primary-50 pointer-events-none',
-        default: 'bg-transparent bg-primary-50 text-primary-900',
-      }
-    },
-    defaultVariants: {
-      state: 'default'
-    },
-  },
-);
-
-function NewFileButton() {
-  const [inputVisible, setInputVisible] = useState(false);
-  const dispatch = useDispatch();
-  if (inputVisible) {
-    return (
-      <form
-        onSubmit={ev => {
-          dispatch(newFile(ev.currentTarget.filename.value));
-          setInputVisible(false);
-        }}
-      >
-        <input
-          type="text"
-          name="filename"
-          placeholder="filename"
-          className="block w-full border-transparent outline-primary-300 p-2 outline outline-1 hover:outline"
-        />
-      </form>
-    );
-  }
-  return (
-    <Button className="w-full" onClick={() => setInputVisible(true)}>
-      New File
-    </Button>
-  );
-}
-
 export function FileBrowser({ className }: { className?: string }) {
-  const files = useSelector(getFileList);
-  // const visible = useSelector(state => state.ui.filesVisible)
-  const { path } = useSelector(getCurrentFile);
+  const [inputVisible, setInputVisible] = useState(false);
+
   const dispatch = useDispatch();
+  const fileList = useSelector(getFileList);
+  const visible = useSelector(state => state.ui.filebarVisible);
+  const { path } = useSelector(getCurrentFile);
+  const files = useMemo(
+    () => fileList.map(file => ({ id: file })),
+    [fileList],
+  );
   return (
     <aside
       className={clsx(
         "space-y-2 px-2",
+        visible ? "block" : "hidden",
         className,
       )}
     >
-      <ul className="bg-white p-2 outline outline-1 outline-primary-300">
-        {files.map(file => (
-          <FileNode
-            key={file}
-            onClick={() => dispatch(currentFileChanged(file))}
-            state={file === path ? 'selected' : 'default'}
+      <ListBox
+        selectionMode="single"
+        aria-label="files in this fiddle"
+        onSelectionChange={selection => {
+          if (typeof selection === "string") {
+            return dispatch(currentFileChanged(selection));
+          }
+          for (const item of selection) {
+            return dispatch(currentFileChanged(item));
+          }
+        }}
+        selectedKeys={[path]}
+        className="bg-white p-2 text-sm outline outline-1 outline-primary-300"
+        items={files}
+      >
+
+        {file => (
+          <ListBoxItem
+            className={({ isSelected }) =>
+              clsx(
+                isSelected
+                  ? "pointer-events-none bg-primary-700 text-primary-50"
+                  : "bg-primary-50 bg-transparent text-primary-900",
+                "tracking-light block w-full p-1 text-left text-sm outline-1 outline-primary-300 hover:outline",
+              )
+            }
+            key={file.id}
           >
-            {file}
-          </FileNode>
-        ))}
-      </ul>
-      <NewFileButton />
+            {file.id}
+          </ListBoxItem>
+        )}
+
+      </ListBox>
+      {inputVisible ? (
+        <form
+          onSubmit={ev => {
+            const name = ev.currentTarget.filename.value;
+            if (name?.length > 0) {
+              dispatch(newFile(name));
+            }
+            setInputVisible(false);
+          }}
+        >
+          <Input
+            type="text"
+            autoFocus
+            name="filename"
+            placeholder="filename"
+            className="block w-full border-transparent p-2 outline outline-1 outline-primary-300 hover:outline"
+          />
+        </form>
+      ) : (
+        <Button className="w-full" onPress={() => setInputVisible(true)}>
+          New File
+        </Button>
+      )}
     </aside>
   );
 }
