308f9fdcf31bad99caa79529938410256b9ade41
DanielFGray <danielfgray@gmail.com>
2024-07-15 10:18:51 -0500

    add: run worker in separate process

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,7 @@
 import "./assertEnv";
 import { app } from "./app";
-import { createWorker } from "./worker";
 
 app.listen(process.env.PORT);
-
-createWorker().catch(err => {
-  console.error(err);
-  process.exit(1);
-});
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port} `,
